0c2f3f46696f8b2a7196359909f8d60a50b890ab
EC2 Default User <ec2-user@ip-172-31-20-213.ap-southeast-1.compute.internal>
2019-09-24 05:14:01 +0000

    Can always fetch Pikachu

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+/* global axios */
+/* global $ */
+
+const api_url = "https://pokeapi.co/api/v2/pokemon/pikachu/";
+
+// This is for testing only!
 function testAPI()
 {
     axios.get("https://pokeapi.co/api/v2/pokemon/pikachu/")
         .then(function(response){
-            console.log(response)
+            console.log(response);
+            console.log(response.data.abilities);
+            console.log(response.data.abilities[0]);
+            console.log(response.data.abilities[0].ability);
         })
     
 }
 
 $(function(){
-    
+    $("#find-button").click(function(){
+        axios.get(api_url).
+        then(function(response){
+            let pokemon = response.data;
+            $("#pokemon-name").text(pokemon.name);
+            $("#pokemon-image").attr('src', pokemon.sprites.front_shiny);
+            $("#experience").text(pokemon.base_experience);
+            
+            let abilities = response.data.abilities;
+            for (let a of abilities)
+            {
+                console.log(a);
+                $("#abilities").append($("<li>" + a.ability.name + "</li>"));
+            }
+        });
+    })
 })
