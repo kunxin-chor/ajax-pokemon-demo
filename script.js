d6bb87778a4dc44a0ed200425b180e54d05e2134
EC2 Default User <ec2-user@ip-172-31-20-213.ap-southeast-1.compute.internal>
2019-09-24 06:36:59 +0000

    Can search for pokemon by name

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* global axios */
 /* global $ */
 
-const api_url = "https://pokeapi.co/api/v2/pokemon/pikachu/";
+const api_url = "https://pokeapi.co/api/v2/";
 
 // This is for testing only!
 function testAPI()
@@ -18,13 +18,16 @@ function testAPI()
 
 $(function(){
     $("#find-button").click(function(){
-        axios.get(api_url).
+        let searchTerms = $("#search").val();
+    
+        axios.get(api_url + "pokemon/" + searchTerms).
         then(function(response){
             let pokemon = response.data;
             $("#pokemon-name").text(pokemon.name);
             $("#pokemon-image").attr('src', pokemon.sprites.front_shiny);
             $("#experience").text(pokemon.base_experience);
             
+            $("#abilities").empty();
             let abilities = response.data.abilities;
             for (let a of abilities)
             {
